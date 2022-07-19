183402c75de4961b15ca8431ce8c0af986dc058b
weixiaojin0712 <101002463+weixiaojin0712@users.noreply.github.com>
2022-07-19 13:10:59 +0000

    Deploying to gh-pages from  @ 9ea5109405c6b60d3cfe1426929b10a223a28c4d 🚀

---
+++ assets/01_install.html.d176a965.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c as s,a}from"./app.879cb312.js";const _={},o=a("p",null,"wget http://archive.apache.org/dist/mesos/1.7.1/mesos-1.7.1.tar.gz",-1),c=[o];function r(l,n){return t(),s("div",null,c)}var d=e(_,[["render",r],["__file","01_install.html.vue"]]);export{d as default};
